88645d0e527b7ef55d2b7011dedc63695fc59f90
Shelby Clayton and Vanessa Palacios Sharma <vanessa@>
2016-10-31 11:00:20 -0700

    Add new-post and post-tile.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,15 @@
 import Ember from 'ember';
 
 
-
 export default Ember.Route.extend({
+  model()
+    return this.store.findAll('post');
+  },
+
+  actions: {
+    destroyPost(post) {
+      post.destroyPost();
+      this.transitionTo('index');
+    }
+  }
 });
